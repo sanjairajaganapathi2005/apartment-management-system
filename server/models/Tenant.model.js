1cb4198adc481ab36bca519290965f31fb2e2c73
Sreepriyanth <125353490+Sreepriyanth2005@users.noreply.github.com>
2025-05-02 11:05:21 +0530

    owner update put delete

--- server/models/Tenant.model.js
+++ server/models/Tenant.model.js
@@ -3,12 +3,12 @@
 const mongoose = require("mongoose");
 
 const TenantSchema = new mongoose.Schema({
-  tenantno: { type: String, required: true, unique: true },
-  name: String,
-  dob: Date,
-  roomNo: String,
-  age: Number,
-  adhaar: String
+  tenantId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  dob: { type: Date, required: true },
+  roomNo: { type: String, required: true },
+  age: { type: Number, required: true },
+  adhaar: { type: String, required: true, unique: true },
 });
 
 module.exports = mongoose.model("Tenant", TenantSchema);
