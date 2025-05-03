fc6e8f42707e316f3e7afaeb45a0e72e5884a88e
Sreepriyanth <125353490+Sreepriyanth2005@users.noreply.github.com>
2025-05-03 10:28:50 +0530

    tenant model update

--- server/models/Tenant.model.js
+++ server/models/Tenant.model.js
@@ -6,9 +6,10 @@ const TenantSchema = new mongoose.Schema({
   tenantId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   dob: { type: Date, required: true },
-  roomNo: { type: String, required: true },
-  age: { type: Number, required: true },
   adhaar: { type: String, required: true, unique: true },
+  agreement: { type: String, required: true }, // Assuming this stores agreement details or a file reference
+  status: { type: String, enum: ["active", "inactive"], default: "active" },
+  blockNo: { type: String, required: true }
 });
 
 module.exports = mongoose.model("Tenant", TenantSchema);
